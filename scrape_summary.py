77e0dbef0cd23f2133fed9aa8a3724055717d25a
r-williamson <rwilliamson228@gmail.com>
2023-04-04 12:56:28 -0600

    Changed data structures to be larger dicts

--- scrape_summary.py
+++ scrape_summary.py
@@ -2,6 +2,9 @@ import json
 import requests
 from bs4 import BeautifulSoup, NavigableString
 import re
+import csv
+import pandas as pd
+
 
 # TODO: implement a loop to scrape all NFL team summary pages, or let user choose the team
 
@@ -27,10 +30,12 @@ def scrape_intro_page():
     summary_html = team_info_soup.div.find_all("p")
 
     franchise_summary = []
+    stat_dict = {}
     for item in summary_html[2:]:
         stat = re.sub("[\n\t]", "", item.text).split(":")
-        stat_dict = {stat[0]: stat[1]}
-        franchise_summary.append(stat_dict)
+        stat_dict[stat[0]] = stat[1]
+        # stat_dict = {stat[0]: stat[1]}
+    franchise_summary.append(stat_dict)
 
     # Scrape Franchise Retired Numbers
     retired_nums_soup = soup.find(class_="uni_holder pfr section")
@@ -47,13 +52,15 @@ def scrape_intro_page():
         if type(row) is not NavigableString:
             if len(row.th["class"]) > 1:  # filler rows have multiple CSS classes declared for col heads and are skipped
                 continue
-            year_stats = []
+            # year_stats = []
+            year_stats = {}
             for col in row:
                 if type(col) is not NavigableString:
                     stat = col.attrs['data-stat']
                     val = col.text
                     col_dict = {stat: val}
-                    year_stats.append(col_dict)
+                    # year_stats.append(col_dict)
+                    year_stats[stat] = val
 
             franchise_stats.append(year_stats)
 
@@ -68,6 +75,11 @@ def scrape_intro_page():
 
 if __name__ == "__main__":
     intro_page = scrape_intro_page()
+
+    # data as pandas df:
+    df = pd.DataFrame.from_dict(intro_page['Franchise Stats'])
+    df.set_index('year_id', inplace=True)
+    print(df)
 
     # dump to json
     with open('result.json', 'w') as file:
